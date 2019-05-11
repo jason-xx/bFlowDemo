dada2f6aac7a33af0a021139f50bfcd697e0938c
jiawenxiao <jiawenxiao@thit.com.cn>
2019-05-11 17:56:44 +0800

    init service

---
+++ bfservice/src/main/java/bfdemo/bfservice/BfserviceApplication.java
@@ -0,0 +1,13 @@
+package bfdemo.bfservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfserviceApplication.class, args);
+	}
+
+}
